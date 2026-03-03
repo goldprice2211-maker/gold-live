fbffd30026f0df28e0c0d49c5d566ac3d805cf83
goldprice2211-maker <goldprice2211@gmail.com>
2026-03-03 06:24:55 +0400

    seo qatar page

--- app/qatar/page.tsx
+++ app/qatar/page.tsx
@@ -1,5 +1,11 @@
 import Home from "../page";
 
+export const metadata = {
+  title: "سعر الذهب اليوم في قطر - تحديث مباشر",
+  description:
+    "أسعار الذهب اليوم في قطر لجميع العيارات (24، 22، 21، 18) بالريال القطري مع تحديث مباشر.",
+};
+
 export default function QatarPage() {
   return <Home />;
 }
