b39d5c0d19731adb5c70375faec46d551e890d92
goldprice2211-maker <goldprice2211@gmail.com>
2026-03-12 20:05:08 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({
       </head>
 
       <body
-        className={${geistSans.variable} ${geistMono.variable} antialiased}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
 
